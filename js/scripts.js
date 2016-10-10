4abe8f4f9b45bea89f122fc34cad1242a36acbca
zane <i.zane.am.robot@gmail.com>
2016-10-09 20:53:29 -0400

    more progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
-/* To do
-
-Render to Dom:
--Display player x or o
--Initialize 3x3 tic tac toe board
--Each board will be a div with class square
-
-
--Conditional statement to check if there are either three x's or o's
-*/
-
 var ticTacToe = {
 
-	//Record whose Turn it is
+	//Record whose Turn it is. Initial starting value is X
 	turn: "X",
-	//Decides if there is a winner or not
-	winner: false,
-	//Keeps Record of Board on Click
-	board: ["", "", "", "", "", "", "", "", ""],
-	//Possible Win Patterns
-	wins: [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]],
+	//Display Value of Winner
+	winner: "",
+	//Keeps Record of Board Values
+	board: {
+		cell1: "",
+		cell2: "",
+		cell3: "",
+		cell4: "",
+		cell5: "",
+		cell6: "",
+		cell7: "",
+		cell8: "",
+		cell9: "",
+	},
 
 	//Record Number of Turns
 	number_of_turns: 0,
@@ -28,32 +25,36 @@ var ticTacToe = {
 		//Reset Number of Turns
 		this.number_of_turns = 0;
 		//Append Player Score
-		$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
+		$("#menu").empty().append("Player <strong>" + this.turn + "'s</strong> turn!");
 		//Empty the Squares
 		if($("td:not(:empty)").length) {
 			$('td:not(:empty)').empty();
 		}
 	},
 
-	playerChoice: function() {
-		if (ticTacToe.number_of_turns > 8) {
-			if(ticTacToe.turn === "X") {
-				$("td").append("X");
-				this.turn = "O";
-				$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
-				console.log("turn is " + ticTacToe.turn);
-				this.number_of_turns++;
-			} else if(ticTacToe.turn === "O") {
-				$("td").append("O");
-				this.turn = "X";
-				$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
-				console.log("turn is " + ticTacToe.turn);
-				this.number_of_turns++;
-			}
-		//Game is over- all td's are full
-		} else {
-			ticTacToe.startGame();
-			alert("Game is over!");
+	// Check for Winner. We must check if the values of each row are equal using board object
+	//AND if the values have a length greater than one
+	winCheck: function() {
+		//Top Row Across Equal
+		if (this.board.cell1 === this.board.cell2 && this.board.cell1 === this.board.cell3 && this.board.cell2 === this.board.cell3 && this.board.cell1.length > 0 && this.board.cell2.length > 0 && this.board.cell3.length > 0) {
+			this.winner = this.board.cell1;
+			alert(this.winner + " is the winner!");
+		//Second Row Across Equal
+		} else if(this.board.cell4 === this.board.cell5 && this.board.cell4 === this.board.cell6 && this.board.cell5 === this.board.cell6 && this.board.cell4.length > 0 && this.board.cell5.length > 0 && this.board.cell6.length > 0) {
+			this.winner = this.board.cell4;
+			alert(this.winner + " is the winner!");
+		//Thid Row Across
+		} else if(this.board.cell7 === this.board.cell8 && this.board.cell7 === this.board.cell9 && this.cell8 === this.board.cell9 && this.board.cell7.length > 0 && this.board.cell8.length > 0 && this.board.cell9.length > 0) {
+			this.winner = this.board.cell7;
+			alert(this.winner + " is the winner!");
+		//Top left to bottom right diaganol
+		} else if(this.board.cell1 === this.board.cell5 && this.board.cell1 === this.board.cell9 && this.board.cell5 === this.board.cell9 && this.board.cell1.length > 0 && this.board.cell5.length > 0 && this.board.cell9.length > 0) {
+			this.winner = this.board.cell1;
+			alert(this.winner + " is the winner!");
+		//Top Right to bottom Left diaganol
+		} else if(this.board.cell3 === this.board.cell5 && this.board.cell3 === this.board.cell7 && this.board.cell5 === this.board.cell7  && this.board.cell3.length > 0 && this.board.cell5.length > 0 && this.board.cell7.length > 0)  {
+			this.winner = this.board.cell3;
+			alert(this.winner + " is the winner!");
 		}
 	},
 
@@ -69,24 +70,40 @@ $(document).ready(function() {
 });
 
 $("td").click(function() {
-	if (ticTacToe.number_of_turns < 9) {
+	//Make sure all 9 spaces aren't filled
+	if (ticTacToe.number_of_turns <= 9) {
+		ticTacToe.winCheck();
 		if(ticTacToe.turn === "X") {
+			//Add X
 			$(this).append("X");
+			//Disable Ability to Click that Square
+			$(this).unbind();
+			//Change Player Turn
 			ticTacToe.turn = "O";
+			//Update Board Object
+			ticTacToe.board[$(this).attr("id")] = "X";
+			//Update Player Turn on Screen
 			$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
-			console.log("turn is " + ticTacToe.turn);
+			//Keep Track of Number of Turns
 			ticTacToe.number_of_turns++;
 		} else if(ticTacToe.turn === "O") {
+			//Add O
 			$(this).append("O");
+			//Disable Ability to Click that Square
+			$(this).unbind();
+			//Change Player Turn
 			ticTacToe.turn = "X";
+			//Update Board Object
+			ticTacToe.board[$(this).attr("id")] = "O";
+			//Update Player Turn on Screen
 			$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
 			console.log("turn is " + ticTacToe.turn);
 			ticTacToe.number_of_turns++;
 		}
-		//Game is over- all td's are full
-		} else {
-			ticTacToe.startGame();
-			alert("Game is over!");
-		}
-	});
+	console.log("Total # of turns: " + ticTacToe.number_of_turns);
+	//All Spaces are filled, but no winner
+	} else {
 
+	}	
+
+});
