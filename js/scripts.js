059a6bcf5f4bea0a624e2bb5f8917bc56fa05829
zane <i.zane.am.robot@gmail.com>
2016-10-10 23:02:03 -0400

    almost finished

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ var ticTacToe = {
 	turn: "X",
 	//Display Value of Winner
 	winner: "",
+	//Scores (Will add this feature later)
+	x_wins: 0,
+	o_wins: 0,
 	//Keeps Record of Board Values
 	board: {
 		cell1: "",
@@ -17,7 +20,7 @@ var ticTacToe = {
 		cell9: "",
 	},
 
-	//Record Number of Turns
+	//Record Number of Turnset
 	number_of_turns: 0,
 
 	//Function for Initializing Game
@@ -30,6 +33,15 @@ var ticTacToe = {
 		if($("td:not(:empty)").length) {
 			$('td:not(:empty)').empty();
 		}
+	},
+
+	//Function for Restarting Game
+	restartGame: function() {
+		//Reset number of turns
+		this.number_of_turns = 0;
+		//Append Player Score
+		$("#menu").empty().append("Player <strong>" + this.turn + "'s</strong> turn!");
+
 	},
 
 	// Check for Winner. We must check if the values of each row are equal using board object
@@ -46,6 +58,18 @@ var ticTacToe = {
 		//Thid Row Across
 		} else if(this.board.cell7 === this.board.cell8 && this.board.cell7 === this.board.cell9 && this.cell8 === this.board.cell9 && this.board.cell7.length > 0 && this.board.cell8.length > 0 && this.board.cell9.length > 0) {
 			this.winner = this.board.cell7;
+			alert(this.winner + " is the winner!");
+		//First Row Up Down
+		} else if (this.board.cell1 === this.board.cell4 && this.board.cell1 === this.board.cell7 && this.board.cell4 === this.board.cell7 && this.board.cell1.length > 0 && this.board.cell4.length > 0 && this.board.cell7.length > 0 ) {
+			this.winner = this.board.cell1;
+			alert(this.winner + " is the winner!");
+		//Second Row Going Down
+		} else if (this.board.cell2 === this.board.cell5 && this.board.cell2 === this.board.cell8 && this.board.cell5 === this.board.cell8 && this.board.cell2.length > 0 && this.board.cell5.length > 0 && this.board.cell8.length > 0 ) {
+			this.winner = this.board.cell2;
+			alert(this.winner + " is the winner!");
+		//Thid Row Going Down
+		} else if (this.board.cell3 === this.board.cell6 && this.board.cell3 === this.board.cell9 && this.board.cell6 === this.board.cell9 && this.board.cell3.length > 0 && this.board.cell6.length > 0 && this.board.cell9.length > 0 ) {
+			this.winner = this.board.cell3;
 			alert(this.winner + " is the winner!");
 		//Top left to bottom right diaganol
 		} else if(this.board.cell1 === this.board.cell5 && this.board.cell1 === this.board.cell9 && this.board.cell5 === this.board.cell9 && this.board.cell1.length > 0 && this.board.cell5.length > 0 && this.board.cell9.length > 0) {
@@ -69,10 +93,14 @@ $(document).ready(function() {
 	console.log(ticTacToe.turn);
 });
 
+
+/*Note: Currently, if you get three in a row, the code will display
+the win alert AFTER you get three in a row, but not clear the divs or anything.
+*/
+
 $("td").click(function() {
 	//Make sure all 9 spaces aren't filled
 	if (ticTacToe.number_of_turns <= 9) {
-		ticTacToe.winCheck();
 		if(ticTacToe.turn === "X") {
 			//Add X
 			$(this).append("X");
@@ -86,6 +114,7 @@ $("td").click(function() {
 			$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
 			//Keep Track of Number of Turns
 			ticTacToe.number_of_turns++;
+			ticTacToe.winCheck();
 		} else if(ticTacToe.turn === "O") {
 			//Add O
 			$(this).append("O");
@@ -99,6 +128,7 @@ $("td").click(function() {
 			$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
 			console.log("turn is " + ticTacToe.turn);
 			ticTacToe.number_of_turns++;
+			ticTacToe.winCheck();
 		}
 	console.log("Total # of turns: " + ticTacToe.number_of_turns);
 	//All Spaces are filled, but no winner
@@ -107,3 +137,4 @@ $("td").click(function() {
 	}	
 
 });
+
