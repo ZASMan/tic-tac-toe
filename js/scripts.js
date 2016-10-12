4395a162eb8094cef22411fe2eef729111016d5a
zane <i.zane.am.robot@gmail.com>
2016-10-11 20:19:39 -0400

    almost finished

--- js/scripts.js
+++ js/scripts.js
@@ -27,20 +27,44 @@ var ticTacToe = {
 	startGame: function() {
 		//Reset Number of Turns
 		this.number_of_turns = 0;
-		//Append Player Score
-		$("#menu").empty().append("Player <strong>" + this.turn + "'s</strong> turn!");
-		//Empty the Squares
-		if($("td:not(:empty)").length) {
-			$('td:not(:empty)').empty();
-		}
-	},
+		//Append Player Turn and Score
+		$("#player-turn").empty().append("Player <strong>" + this.turn + "'s</strong> turn!");
+		$("#player-score").append("Player <strong>X</strong> score: " + this.x_wins + "<br>" +
+			"Player <strong> Y</strong> score: " + this.o_wins);
+      },
 
 	//Function for Restarting Game
 	restartGame: function() {
 		//Reset number of turns
 		this.number_of_turns = 0;
+		//Reset Player Turn
+		this.turn = "X";
+		$("#player-turn").empty().append("Player <strong>" + this.turn + "'s</strong> turn!");
 		//Append Player Score
-		$("#menu").empty().append("Player <strong>" + this.turn + "'s</strong> turn!");
+		$("#player-score").empty().append("Player <strong>X</strong> score: " + this.x_wins + "<br>" +
+			"Player <strong> Y</strong> score: " + this.o_wins);
+		//Recreate Elements (Need to find a way to delete/recreate elements to account for
+		//the originally removed click binds)
+		$("#board").remove();
+		$("#game").append('<div id="board"><table class="table-bordered" id="game-table">' +
+			'<tbody>' +
+             '<tr>'+
+                '<td id="cell1"></td>'+
+                '<td id="cell2"></td>' +
+                '<td id="cell3"></td>' +
+              '</tr>' +
+              '<tr>'+
+                '<td id="cell4"></td>'+
+                '<td id="cell5"></td>'+
+                '<td id="cell6"></td>'+
+              '</tr>'+
+              '<tr>'+
+                '<td id="cell7"></td>'+
+                '<td id="cell8"></td>'+
+                '<td id="cell9"></td>'+
+              '</tr>'+
+            '</tbody>'+
+          '</table></div>');
 
 	},
 
@@ -51,34 +75,47 @@ var ticTacToe = {
 		if (this.board.cell1 === this.board.cell2 && this.board.cell1 === this.board.cell3 && this.board.cell2 === this.board.cell3 && this.board.cell1.length > 0 && this.board.cell2.length > 0 && this.board.cell3.length > 0) {
 			this.winner = this.board.cell1;
 			alert(this.winner + " is the winner!");
+			if (this.winner === "X") {
+				this.x_wins++;
+			} else if (this.winner === "O") {
+				this.o_wins++;
+			}
+			this.restartGame();
 		//Second Row Across Equal
 		} else if(this.board.cell4 === this.board.cell5 && this.board.cell4 === this.board.cell6 && this.board.cell5 === this.board.cell6 && this.board.cell4.length > 0 && this.board.cell5.length > 0 && this.board.cell6.length > 0) {
 			this.winner = this.board.cell4;
 			alert(this.winner + " is the winner!");
+			this.restartGame();
 		//Thid Row Across
 		} else if(this.board.cell7 === this.board.cell8 && this.board.cell7 === this.board.cell9 && this.cell8 === this.board.cell9 && this.board.cell7.length > 0 && this.board.cell8.length > 0 && this.board.cell9.length > 0) {
 			this.winner = this.board.cell7;
 			alert(this.winner + " is the winner!");
+			this.restartGame();
 		//First Row Up Down
 		} else if (this.board.cell1 === this.board.cell4 && this.board.cell1 === this.board.cell7 && this.board.cell4 === this.board.cell7 && this.board.cell1.length > 0 && this.board.cell4.length > 0 && this.board.cell7.length > 0 ) {
 			this.winner = this.board.cell1;
 			alert(this.winner + " is the winner!");
+			this.restartGame();
 		//Second Row Going Down
 		} else if (this.board.cell2 === this.board.cell5 && this.board.cell2 === this.board.cell8 && this.board.cell5 === this.board.cell8 && this.board.cell2.length > 0 && this.board.cell5.length > 0 && this.board.cell8.length > 0 ) {
 			this.winner = this.board.cell2;
 			alert(this.winner + " is the winner!");
+			this.restartGame();
 		//Thid Row Going Down
 		} else if (this.board.cell3 === this.board.cell6 && this.board.cell3 === this.board.cell9 && this.board.cell6 === this.board.cell9 && this.board.cell3.length > 0 && this.board.cell6.length > 0 && this.board.cell9.length > 0 ) {
 			this.winner = this.board.cell3;
 			alert(this.winner + " is the winner!");
+			this.restartGame();
 		//Top left to bottom right diaganol
 		} else if(this.board.cell1 === this.board.cell5 && this.board.cell1 === this.board.cell9 && this.board.cell5 === this.board.cell9 && this.board.cell1.length > 0 && this.board.cell5.length > 0 && this.board.cell9.length > 0) {
 			this.winner = this.board.cell1;
 			alert(this.winner + " is the winner!");
+			this.restartGame();
 		//Top Right to bottom Left diaganol
 		} else if(this.board.cell3 === this.board.cell5 && this.board.cell3 === this.board.cell7 && this.board.cell5 === this.board.cell7  && this.board.cell3.length > 0 && this.board.cell5.length > 0 && this.board.cell7.length > 0)  {
 			this.winner = this.board.cell3;
 			alert(this.winner + " is the winner!");
+			this.restartGame();
 		}
 	},
 
@@ -93,11 +130,6 @@ $(document).ready(function() {
 	console.log(ticTacToe.turn);
 });
 
-
-/*Note: Currently, if you get three in a row, the code will display
-the win alert AFTER you get three in a row, but not clear the divs or anything.
-*/
-
 $("td").click(function() {
 	//Make sure all 9 spaces aren't filled
 	if (ticTacToe.number_of_turns <= 9) {
@@ -111,7 +143,7 @@ $("td").click(function() {
 			//Update Board Object
 			ticTacToe.board[$(this).attr("id")] = "X";
 			//Update Player Turn on Screen
-			$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
+			$("#player-turn").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
 			//Keep Track of Number of Turns
 			ticTacToe.number_of_turns++;
 			ticTacToe.winCheck();
@@ -125,7 +157,7 @@ $("td").click(function() {
 			//Update Board Object
 			ticTacToe.board[$(this).attr("id")] = "O";
 			//Update Player Turn on Screen
-			$("#menu").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
+			$("#player-turn").empty().append("Player <strong>" + ticTacToe.turn + "'s</strong> turn!");
 			console.log("turn is " + ticTacToe.turn);
 			ticTacToe.number_of_turns++;
 			ticTacToe.winCheck();
@@ -133,7 +165,8 @@ $("td").click(function() {
 	console.log("Total # of turns: " + ticTacToe.number_of_turns);
 	//All Spaces are filled, but no winner
 	} else {
-
+		alert("Sorry, there is no winner! Try again");
+		ticTacToe.restartGame();
 	}	
 
 });
